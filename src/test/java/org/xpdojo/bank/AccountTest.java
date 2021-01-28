1c38613e1497c530ba8e3d0145cdc420aa487fa2
Abhishek Kumar <gabhishekkumar@outlook.com>
2021-01-28 20:42:20 +0530

    Added TODO scope in junits

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -12,4 +12,13 @@ public class AccountTest {
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test").isBlank();
     }
+
+    // TODO : I can Deposit money to accounts
+
+    // TODO : I can Withdraw money from accounts
+    // TODO : I can Transfer amounts between accounts (if I have the funds)
+    // TODO : I can print out an Account balance slip (date, time, balance)
+    // TODO : I can print a statement of account activity (statement)
+    // TODO : I can apply Statement filters (include just deposits, withdrawal, date)
+
 }
